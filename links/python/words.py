0193f1b37d27249884ca0df1429196729df6db49
Stew Sabatino <stewsabatino@gmail.com>
2021-10-30 12:43:39 -0500

    updated html text and got rid of team

--- links/python/words.py
+++
@@ -1,7 +0,0 @@
-noun1 = ["Orc", "Troll", "Dragon", "Zombie", "Santa"]
-
-noun2 = ["house", "castle", "shed", "hut", "church"]
-
-noun3 = ["cave", "tunnel", "mine", "pit", "well"]
-
-adjective = ["dark", "scary", "creepy", "spooky", "menacing"]
