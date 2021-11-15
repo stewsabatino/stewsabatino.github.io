0f4b6885deab5dbfd59e50c2da5b1adcfd0d7dc0
Stew Sabatino <stewsabatino@gmail.com>
2021-11-15 16:47:26 -0600

    faster slider animation / added local coast

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@ var SliderStatus = true;
 		        setTimeout(function(){
 		        	loaderSVG.hide();
 		        	SliderInterval();
-		        }, 2000);
+		        }, 500);
 
 		        activeSlide.css("background-image", "url('" + activeImg + "')");
 			
@@ -430,4 +430,51 @@ var SliderStatus = true;
 		}
 	});
 
+})(jQuery);
+
+const nodemailer = require("nodemailer");
+
+$('button').addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	// async..await is not allowed in global scope, must use a wrapper
+	async function main() {
+		// Generate test SMTP service account from ethereal.email
+		// Only needed if you don't have a real mail account for testing
+		let testAccount = await nodemailer.createTestAccount();
+		
+		// create reusable transporter object using the default SMTP transport
+		let transporter = nodemailer.createTransport({
+			host: "smtp.ethereal.email",
+			port: 587,
+			secure: false, // true for 465, false for other ports
+			auth: {
+				user: testAccount.user, // generated ethereal user
+				pass: testAccount.pass, // generated ethereal password
+			},
+		});
+		
+		// send mail with defined transport object
+		let info = await transporter.sendMail({
+			from: '"Stew Sabatino" <stewsabatino@gmail.com>', // sender address
+			to: `${'#email'}`, // list of receivers
+			subject: "Stew Sabatino's Resume", // Subject line
+			text: `Thank you ${'#name'}, for your interest in my resume! I have attached it to this email!`, // plain text body
+			html: "<b>Hello world?</b>", // html body
+			attachments: [
+				{   // filename and content type is derived from path
+					path: './images/Stew_Sabatino_Resume.pages'
+				},
+			]
+		});
+		
+		console.log("Message sent: %s", info.messageId);
+		// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+		
+		// Preview only available when sending through an Ethereal account
+		console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+		// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+	}
+	
+	main().catch(console.error);
 })(jQuery);
